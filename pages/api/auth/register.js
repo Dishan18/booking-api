eb9ea29345b360e10f32639d8c396ba839d1f1d4
DishanSarkar <dishansrkr@gmail.com>
2026-04-25 16:53:33 +0530

    changed id to cryptouuid for 0 collision

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,30 +1,24 @@
 import { db } from "@/lib/db";
 import bcrypt from "bcryptjs";
-
+import crypto from "crypto";
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).end();
   }
-
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.status(400).json({ error: "Missing fields" });
   }
-
   if (db.users.find((u) => u.email === email)) {
     return res.status(400).json({ error: "User exists" });
   }
-
   const hashed = await bcrypt.hash(password, 10);
-
   const user = {
-    id: Date.now().toString(),
+    id: crypto.randomUUID(),
     name,
     email,
     password: hashed,
   };
-
   db.users.push(user);
-
   res.status(201).json({ message: "Registered" });
 }
